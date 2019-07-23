df9a94f4f5210e53d86cb50b3ce9030a980c2ec0
apprnk <52127040+apprnk@users.noreply.github.com>
2019-07-23 07:57:16 +0700

    Create function.php

---
+++ function.php
@@ -0,0 +1,18 @@
+<?php 
+function array_multisort_value()
+{
+    $args = func_get_args();
+    $data = array_shift($args);
+    foreach ($args as $n => $field) {
+        if (is_string($field)) {
+            $tmp = array();
+            foreach ($data as $key => $row) {
+                $tmp[$key] = $row[$field];
+            }
+            $args[$n] = $tmp;
+        }
+    }
+    $args[] = &$data;
+    call_user_func_array('array_multisort', $args);
+    return array_pop($args);
+}
